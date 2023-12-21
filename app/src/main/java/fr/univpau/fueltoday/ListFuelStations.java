96ec5abb52c5aca8f5323424c41bac5a9917651f
Arnaud <arnaud.chaubet@outlook.com>
2023-12-21 14:16:51 +0100

    update: updated Station and ListFuelStations

--- app/src/main/java/fr/univpau/fueltoday/ListFuelStations.java
+++ app/src/main/java/fr/univpau/fueltoday/ListFuelStations.java
@@ -8,7 +8,9 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class ListFuelStations {
     private Activity activity;
@@ -31,7 +33,42 @@ public class ListFuelStations {
             String pop = firstResult.getString("pop");
             String address = firstResult.getString("adresse");
             String city = firstResult.getString("ville");
-            Station station = new Station(id, latitude, longitude, cp, pop, address, city);
+
+            String prixString = firstResult.getString("prix");
+            JSONArray prixArray = new JSONArray(prixString);
+
+            double sp95_prix = 0.0;
+            double e10_prix = 0.0;
+            double sp98_prix = 0.0;
+            double gasoilPrix = 0.0;
+
+            for (int j = 0; j < prixArray.length(); j++) {
+                JSONObject prixObject = prixArray.getJSONObject(j);
+                String carburant = prixObject.getString("@nom");
+                double valeur = prixObject.getDouble("@valeur");
+                switch (carburant) {
+                    case "SP95":
+                        sp95_prix = valeur;
+                        break;
+                    case "E10":
+                        e10_prix = valeur;
+                        break;
+                    case "SP98":
+                        sp98_prix = valeur;
+                        break;
+                    case "Gazole":
+                        gasoilPrix = valeur;
+                        break;
+                }
+            }
+
+            JSONArray servicesArray = firstResult.getJSONArray("services_service");
+            Set<String> servicesSet = new HashSet<>();
+            for (int j = 0; j < servicesArray.length(); j++) {
+                servicesSet.add(servicesArray.getString(j));
+            }
+
+            Station station = new Station(id, latitude, longitude, cp, pop, address, city, sp95_prix, e10_prix, sp98_prix, gasoilPrix, servicesSet);
             stationList.add(station);
         }
         StationAdapter adapter = new StationAdapter(activity, stationList);
--- app/src/main/java/fr/univpau/fueltoday/Station.java
+++ app/src/main/java/fr/univpau/fueltoday/Station.java
@@ -1,5 +1,7 @@
 package fr.univpau.fueltoday;
 
+import java.util.Set;
+
 public class Station {
     public int id;
     public double latitude;
@@ -8,7 +10,12 @@ public class Station {
     public String pop;
     public String address;
     public String city;
-    public Station(int id, double latitude, double longitude, String cp, String pop, String address, String city) {
+    public double sp95_prix;
+    public double e10_prix;
+    public double sp98_prix;
+    public double gasoil_prix;
+    public Set<String> services;
+    public Station(int id, double latitude, double longitude, String cp, String pop, String address, String city, double sp95_prix, double e10_prix, double sp98_prix, double gasoil_prix, Set<String> services) {
         this.id = id;
         this.latitude = latitude;
         this.longitude = longitude;
@@ -16,6 +23,11 @@ public class Station {
         this.pop = pop;
         this.address = address;
         this.city = city;
+        this.sp95_prix = sp95_prix;
+        this.e10_prix = e10_prix;
+        this.sp98_prix = sp98_prix;
+        this.gasoil_prix = gasoil_prix;
+        this.services = services;
     }
 
 }
